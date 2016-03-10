bdd4498e7c9ad0c2391813551f998c13d94e5a3a
Diego G <dgf-95@hotmail.com>
2016-03-09 23:16:56 -0600

    Cambios y validaciones en la base de datos

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,4 @@
 class Transaction < ActiveRecord::Base
+	validates :product_req_id, presence: true
+	validates :product_offered_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	validates :username, presence: true, uniqueness: true
+	validates :password, presence: true, length: { minimum: 3 }
+	has_secure_password
 end
